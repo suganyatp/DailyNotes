d28edc726f7d759d700440dbc4ed6a45d2c825a4
Suganyaa Parivallal <suganyatp@gmail.com>
2026-09-04 18:59:25 +0530

    Create javaScriptBasics.js
    Basics of JS

---
+++ Week1/Day2/javaScriptBasics.js
@@ -0,0 +1,15 @@
+/* Paragraph Comment - Shift+Alt+A
+
+console.log("Step1")
+setTimeout(()=>{console.log("Step2")},3000)
+console.log("Step3") 
+ */
+
+// Single line comment - Ctrl+/
+
+/* Format File = Shift+Alt+F
+
+console.log("Welcome");
+console.log("to");
+console.log("Playwright");
+ */
