ccc9300138cd2d5e12df8c24845819abfc96de88
Suganyaa Parivallal <suganyatp@gmail.com>
2026-09-04 19:02:53 +0530

    Create conditionStatements.js
    Condition Statements in JS

---
+++ Week1/Day3/conditionStatements.js
@@ -0,0 +1,32 @@
+/* Control Statements - controls the execution / flow of the code execution 
+
+1. Conditional Statements
+if
+if-else
+else-if-else
+switch
+
+2. Looping / iteration statemets
+
+for
+while
+do-while
+
+3. Jump statements
+
+break
+continue
+*/
+
+// if condition
+
+let marks=40
+if(marks>80) {
+    console.log("Distinction");
+}else if(marks>60) {
+    console.log("first class");
+}else if(marks>40){
+    console.log("second class");
+} else {
+    console.log("failed");
+}
